1831d07ba4690c79040e7458532a34200d9129d9
tomishninja <tomishninja@gmail.com>
2021-09-11 20:47:25 +0930

    fixed issues regarding unity serilizer incompatablity

--- PVMToJSON/PVMJSONOutput.h
+++ PVMToJSON/PVMJSONOutput.h
@@ -10,59 +10,104 @@ private:
 	unsigned int height;
 	unsigned int breath;
 	unsigned int component;
+	unsigned int scalex;
+	unsigned int scaley;
+	unsigned int scalez;
 
 	unsigned char * volume;
 
 public:
-	const std::string NEWLINE = "\n";
+	const std::string NEWLINE = "";
 
-	PVMJSONOutput(unsigned int width, unsigned int height, unsigned int breath, unsigned int component, unsigned char* volume) {
+	PVMJSONOutput(unsigned int width, unsigned int height, unsigned int breath, unsigned int component, unsigned char* volume, 
+		unsigned int scalex, unsigned int scaley, unsigned int scalez) {
 		this->breath = breath;
 		this->height = height;
 		this->width = width;
 		this->component = component;
 		this->volume = volume;
+		this->scalex = scalex;
+		this->scaley = scaley;
+		this->scalez = scalez;
 	}
 
-	std::string ToJSON(){
+	std::string ToJSON(bool includeScale){
 		// The string continating the json
-		std::string output = "{" + NEWLINE;
+		std::string output = "{";
+		output.append(NEWLINE);
 		
 		// write the volume details
-		output.append("\"width\" : ");
+		output.append("\"width\":");
 		output.append(std::to_string(width));
 		output.append(",");
 		output.append(NEWLINE);
 
-		output.append("\"height\" : ");
+		output.append("\"height\":");
 		output.append(std::to_string(height));
 		output.append(",");
 		output.append(NEWLINE);
 
-		output.append("\"breath\" : ");
+		output.append("\"breath\":");
 		output.append(std::to_string(breath));
 		output.append(",");
 		output.append(NEWLINE);
 
-		output.append("\"components\" : ");
+		/*
+		output.append("\"components\":");
 		output.append(std::to_string(component));
 		output.append(",");
 		output.append(NEWLINE);
+		*/
+
+		if (includeScale) {
+			output.append("\"scalex\":");
+			output.append(std::to_string(scalex));
+			output.append(",");
+			output.append(NEWLINE);
+
+			output.append("\"scaley\":");
+			output.append(std::to_string(scaley));
+			output.append(",");
+			output.append(NEWLINE);
+
+			output.append("\"scaley\":");
+			output.append(std::to_string(scaley));
+			output.append(",");
+			output.append(NEWLINE);
+		}
+
+		for (int index = 0; index < 100; index++) {
+			std::cout << std::to_string(volume[index]) << std::endl;
+		}
 
 		// add the volume data
 		output.append("\"data\": [");
 		int endofLoop = width * height * breath * component;
-		for (int index = 0; index < endofLoop; index++) {
-			output.append(std::to_string(volume[index]));
-			if (index < endofLoop - 1) {
+		for (int index = 0; index < endofLoop; index += component) {
+
+			// add the correct amount of components to the loop
+			switch (component) {
+			case 1 : 
+				output.append(std::to_string(volume[index]));
+				break;
+			case 2:
+				output.append(std::to_string(((unsigned)volume[index] << 8) + (unsigned)volume[index + 1]));
+				break;
+			default:
+				std::cout << "Not Implmented" << std::endl;
+				break;
+			}
+
+			if (index < endofLoop - component) {
 				output.append(",");
 			}
 			else {
-				output.append("],");
+				output.append("]");
 			}
 		}
 		output.append(NEWLINE);
 		output.append("}");
+		output.append("\n");
 
 		return(output);
 	}
--- PVMToJSON/PVMToJSON.cpp
+++ PVMToJSON/PVMToJSON.cpp
@@ -12,7 +12,7 @@
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Starting Processing!\n";
 
 	unsigned char *volume;
 
@@ -32,17 +32,20 @@ int main(int argc, char *argv[])
 	if ((volume = reader.readPVMvolume(argv[1], &width, &height, &depth, &components, &scalex, &scaley, &scalez)) == NULL) exit(1);
 	if (volume == NULL) exit(1);
 
-	
-
+	// if the users have a output dir then it can be added
 	if (argc > 2)
 	{
-		PVMJSONOutput json = PVMJSONOutput(width, height, depth, components, volume);
-		std::string output = json.ToJSON();
+		PVMJSONOutput json = PVMJSONOutput(width, height, depth, components, volume, scalex, scaley, scalez);
+		std::string output = json.ToJSON(true);
 
 		std::ofstream myfile;
 		myfile.open(argv[2], std::ios::out);
 		myfile << output;
 		myfile.close();
+
+		//std::cout << scalex << std::endl;
+		//std::cout << scaley << std::endl;
+		//std::cout << scalez << std::endl;
 
 		//printf("writing RAW file with size=%d\n", width*height*depth*components);
 
@@ -51,27 +54,7 @@ int main(int argc, char *argv[])
 			//true);
 	}
 
-	//std::cout << "width" << width << std::endl;
-	//std::cout << "height" << height << std::endl;
-	//std::cout << "depth" << depth << std::endl;
-	//std::cout << "components" << components << std::endl;
-
-	//std::string vol = std::string(volume);
-	//for (int index = 0; index < 8388608; index++)
-		//std::cout << +volume[index] << ", ";
-
 	free(volume);
 
 	return(0);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
